b563486ff560b0a5212f6eded80fa9fe8e7a3847
Saroj Padhan <40264980+saroj8455@users.noreply.github.com>
2025-07-26 23:04:23 +0530

    implement coverage for config service

--- src/app/services/config.service.spec.ts
+++ src/app/services/config.service.spec.ts
@@ -1,16 +1,37 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { ConfigService } from './config.service';
 
 describe('ConfigService', () => {
   let service: ConfigService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [ConfigService]
+    });
     service = TestBed.inject(ConfigService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should fetch products from fake API', () => {
+    const mockProducts = [{ id: 1, title: 'Product 1' }];
+    service.loadProductsFromFakeApi().subscribe(products => {
+      expect(products).toEqual(mockProducts);
+    });
+
+    const req = httpMock.expectOne('https://fakestoreapi.com/products');
+    expect(req.request.method).toBe('GET');
+    req.flush(mockProducts);
   });
 });
